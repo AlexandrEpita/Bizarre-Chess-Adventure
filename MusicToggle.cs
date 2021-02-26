ebfad7dc81c050725bcc208c080a5f83cd6ac84d
SpisysS <77295360+SpisysS@users.noreply.github.com>
2021-02-26 15:16:39 +0100

    Add files via upload
    music toggle c'est le boutton on/off de la musique
    retour un boutton de retour
    startbutton le button pour commmencer une partie

---
+++ MusicToggle.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicToggle : MonoBehaviour
+{
+    public Text ToggleText;
+    public AudioSource Audio;
+    public void start()
+    {
+        Audio = GetComponent<AudioSource>();
+        
+        if (Audio.isPlaying)
+            {
+                ToggleText.text = "OFF";
+            }
+        else
+            {
+                ToggleText.text = "ON";
+            }
+        
+    }
+    public void MusicToggles()
+    {
+        if (Audio.isPlaying)
+        {
+            Audio.Pause();
+            ToggleText.text = "OFF";
+        }
+        else
+        {
+            Audio.Play();
+            ToggleText.text = "ON";
+        }
+    }
+    void Update()
+    {
+        
+    }
+
+    
+    
+}
